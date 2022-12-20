b2597355d7f1a62236c3b87641c67f4a97818c3b
auriceliof <auricelio.suporte@hotmail.com>
2022-12-20 20:54:19 -0300

    02-10 Padrão de projeto Factory para instanciar objetos

--- aula-junit/src/tests/entities/AccountTests.java
+++ aula-junit/src/tests/entities/AccountTests.java
@@ -2,22 +2,45 @@ package tests.entities;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
 import entities.Account;
+import tests.factory.AccountFactory;
 
 public class AccountTests {
 	
 	@Test
-	public void depositShouldIncreaseBalanceWhenPositiveAmount() {
+	public void depositShouldIncreaseBalanceAndDiscountFeeWhenPositiveAmount() {
 		
 		double amount = 200.0;
 		double expectedValue = 196.0;
-		Account acc = new Account(1L, 0.0);
+		Account acc = AccountFactory.createEmptyAccount();
+		
+		acc.deposit(amount);
+		
+		Assertions.assertEquals(expectedValue, acc.getBalance());
+	}
+	
+	@Test
+	public void depositShouldDoNothingWhenNegativeAmount() {
+		
+		double expectedValue = 100.0;
+		Account acc = AccountFactory.createAccount(expectedValue);
+		double amount = -200.0;
 		
 		acc.deposit(amount);
 		
 		Assertions.assertEquals(expectedValue, acc.getBalance());
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
