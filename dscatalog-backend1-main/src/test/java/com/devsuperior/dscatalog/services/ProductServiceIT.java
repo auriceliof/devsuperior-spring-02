c3e96759fa0294194b22dc9fc0f0c9621c518071
auriceliof <auricelio.suporte@hotmail.com>
2023-01-18 15:30:33 -0300

    02-37 Teste de integração para findAllPaged

--- dscatalog-backend1-main/src/test/java/com/devsuperior/dscatalog/services/ProductServiceIT.java
+++ dscatalog-backend1-main/src/test/java/com/devsuperior/dscatalog/services/ProductServiceIT.java
@@ -5,11 +5,16 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dscatalog.dto.ProductDTO;
 import com.devsuperior.dscatalog.repositories.ProductRepository;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @SpringBootTest
+@Transactional
 public class ProductServiceIT {
 	
 	@Autowired
@@ -43,6 +48,19 @@ public class ProductServiceIT {
 		Assertions.assertThrows(ResourceNotFoundException.class, () -> {			
 			service.delete(nonExistingId);
 		});
+	}
+	
+	@Test
+	public void findAllPagedShouldReturnPageWhenPage0Size10() {
+		
+		PageRequest pageRequest = PageRequest.of(0, 10);
+		
+		Page<ProductDTO> result = service.findAllPaged(pageRequest);
+		
+		Assertions.assertFalse(result.isEmpty());
+		Assertions.assertEquals(0, result.getNumber());		
+		Assertions.assertEquals(10, result.getSize());		
+		Assertions.assertEquals(countTotalProducts, result.getTotalElements());
 	}
 	
 }
